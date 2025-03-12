bbf125009ad5b136b79ea25e794e62492098755e
Thuanne Hannah <thuannehrs@hotmail.com>
2025-03-12 20:17:37 -0300

    mudanca console.log

--- formacao.js
+++ formacao.js
@@ -1,23 +1,22 @@
-let XP = 1500;  
+let XP = 8000;  
 
 if (XP < 1000) {
-    console.log("Ferro");
+    console.log("O Herói Joãozinho está no nível Ferro");
 } else if (XP >= 1001 && XP <= 2000) {
-    console.log("Bronze");
+    console.log("O Herói Joãozinho está no nível Bronze");
 } else if (XP >= 2001 && XP <= 5000) {
-    console.log("Prata");
+    console.log("O Herói Joãozinho está no nível Prata");
 } else if (XP >= 5001 && XP <= 7000) {
-    console.log("Ouro");
+    console.log("O Herói Joãozinho está no nível Ouro");
 } else if (XP >= 7001 && XP <= 8000) {
-    console.log("Platina");
+    console.log("O Herói Joãozinho está no nível Platina");
 } else if (XP >= 8001 && XP <= 9000) {
-    console.log("Ascendente");
+    console.log("O Herói Joãozinho está no nível Ascendente");
 } else if (XP >= 9001 && XP <= 10000) {
-    console.log("Imortal");
+    console.log("O Herói Joãozinho está no nível Imortal");
 } else {
-    console.log("Radiante");
+    console.log("O Herói Joãozinho está no nível Radiante");
 }
 
-console.log("O Herói Joãozinho está no nível de " XP);
 
 
